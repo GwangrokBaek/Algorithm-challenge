351baebdb9455c90b3edbd722d2b85284826bf67
Phil <zester926@gmail.com>
2021-01-10 05:13:01 +0900

    Solve : Fly me to the Alpha Centauri

---
+++ Silver_I/Fly me to the Alpha Centauri.cpp
@@ -0,0 +1,26 @@
+/*
+  Author : Kwangrok Baek (zester926@gmail.com)
+  memory, time : 2028 KB, 0 ms
+  URL : https://www.acmicpc.net/problem/1011
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int TC = 0;
+int x, y, length, d = 0;
+int move_count = 0;
+
+int main(void) {
+    scanf("%d", &TC);
+    for(int i = 0; i < TC; i++) {
+        scanf("%d %d", &x, &y);
+        length = y - x;
+        d = sqrt(length);
+        pow(d, 2) == length ? move_count = 2 * d - 1 : move_count = 2 * d;
+        if (length > d * (d + 1)) move_count += 1;
+        printf("%d\n", move_count);
+    }
+}
