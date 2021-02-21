aa40c69ee869de0f4cab589c59ad052a688d314a
Phil <zester926@gmail.com>
2021-02-22 02:57:25 +0900

    Solved: a_plus_b_minus_3

---
+++ Bronze_III/a_plus_b_minus_3.py
@@ -0,0 +1,9 @@
+#
+# Author : Kwangrok Baek (zester926@gmail.com)
+# URL : https://www.acmicpc.net/problem/10950
+#
+
+n = int(input())
+for _ in range(n):
+    a, b = map(int, input().split())
+    print(a+b)
