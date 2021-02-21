a8f3a19bed514de78547fcdf2ac2575dc40e02ee
Phil <zester926@gmail.com>
2021-02-22 03:03:52 +0900

    Solved: a_plus_b_minus_4

---
+++ Bronze_III/a_plus_b_minus_4.py
@@ -0,0 +1,10 @@
+#
+# Author : Kwangrok Baek (zester926@gmail.com)
+# URL : https://www.acmicpc.net/problem/10951
+#
+
+import sys
+
+for line in sys.stdin:
+    a, b = map(int, line.split())
+    print(a+b)
