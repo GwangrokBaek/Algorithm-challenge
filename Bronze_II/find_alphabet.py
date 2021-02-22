e3d9ff07552667855e2d57402d720e73a648c635
Phil <zester926@gmail.com>
2021-02-22 17:10:48 +0900

    Solved: find_alphabet

---
+++ Bronze_II/find_alphabet.py
@@ -0,0 +1,8 @@
+#
+# Author : Kwangrok Baek (zester926@gmail.com)
+# URL : https://www.acmicpc.net/problem/10809
+#
+
+S = input()
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    print(S.find(i), end=' ')
