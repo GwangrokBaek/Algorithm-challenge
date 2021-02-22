c156035927e3b5c57e8bdf891edaa167a9c473a9
Phil <zester926@gmail.com>
2021-02-22 17:06:25 +0900

    Solved: dog

---
+++ Bronze_V/dog.py
@@ -0,0 +1,10 @@
+#
+# Author : Kwangrok Baek (zester926@gmail.com)
+# URL : https://www.acmicpc.net/problem/10172
+#
+
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print("||_/=\\\\__|")
