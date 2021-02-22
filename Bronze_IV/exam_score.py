c0f6b226ef86e40c95b25edb25860c36e115f4e1
Phil <zester926@gmail.com>
2021-02-22 17:00:56 +0900

    Solved: examp_score

---
+++ Bronze_IV/exam_score.py
@@ -0,0 +1,16 @@
+#
+# Author : Kwangrok Baek (zester926@gmail.com)
+# URL : https://www.acmicpc.net/problem/9498
+#
+
+n = int(input())
+if n >= 90 :
+    print('A')
+elif n >= 80:
+    print('B')
+elif n >= 70:
+    print('C')
+elif n >= 60:
+    print('D')
+else:
+    print('F')
