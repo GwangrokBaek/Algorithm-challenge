ebcab77c12d7322422ac5f4ddbb6ed81d1f47bc1
GwangrokBaek <zester926@gmail.com>
2023-09-13 00:07:18 +0900

    fix: 피보나치 수열 (lv2)

--- programmers/피보나치 수/solution.cpp
+++ programmers/피보나치 수/solution.cpp
@@ -1,22 +1,20 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12945
 
-#include <string>
-#include <vector>
-#include <iostream>
-
 using namespace std;
 
-vector<int> memo[100000];
+int memo[100001] = {0,};
 
 int fibonacci(int n) {
-    if (n <= 2) {
-        return 1;
-    } else {
-        memo[n] = (fibonacci(n - 2) + fibonacci(n - 1));
-        return memo[n];  
+    memo[0] = 0;
+    memo[1] = 1;
+    
+    for (int i = 2; i <= n; i++) {
+        memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
     }
+    
+    return memo[n];
 }
 
 int solution(int n) {
-    return fibonacci(n) % 1234567;
+    return fibonacci(n);
 }
