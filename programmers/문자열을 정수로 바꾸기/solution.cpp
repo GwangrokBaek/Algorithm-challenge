25a5071af1b2f46b5c5227c58123d182e7ca17ff
GwangrokBaek <zester926@gmail.com>
2023-09-22 23:44:45 +0900

    solve: 문자열을 정수로 바꾸기 (lv1)

---
+++ programmers/문자열을 정수로 바꾸기/solution.cpp
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+#include <string>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+
+    answer = stoi(s);
+
+    return answer;
+}
